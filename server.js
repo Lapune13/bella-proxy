da50cc40c14e3403053f5418318cb2a87f4693e4
Lapune13 <lapune13009@gmail.com>
2026-05-17 10:26:57 +0000

    remove dotenv, use Railway env directly

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
